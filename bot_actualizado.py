289ee979302fd5501bafb11f2d892da5803d8381
mariagraciasotelo <161959902+mariagraciasotelo@users.noreply.github.com>
2024-03-23 11:23:50 -0500

    Create bot_actualizado.py

---
+++ bot_actualizado.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+
+bot.run("MTIxODU4NjYyNjU5OTU1NTE2NA.GtTgd4.cv8xH8qI43Z5rbyGs0YLFiPIfoIFYSdegDpqYw")
